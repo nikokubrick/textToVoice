a2e446f247b1d7c8fae95c94439c5269f8388d4e
Saavedra <nicolas.saavedra@sofse.gob.ar>
2019-02-11 17:58:20 -0300

    cambios css

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+var  cadena = "Volve a la escuela! Ah, No, cierto que mauri las cierra";
+var  cadena2 = "te mereces el cielo"
+
 document.getElementById('hablar').addEventListener("click",()=>{
     decir(document.getElementById("texto").value);
+
+
 });
+
+document.getElementById('hablarr').addEventListener("click",()=>{
+    decir(document.getElementById("textok").value);
+
+
+});
+
 
 function decir(texto){
     speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+
 }
